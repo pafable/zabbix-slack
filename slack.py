18ad5b0b4bf6bdea4b50ee3a5a01459b2a5c26ee
pafable <39135798+pafable@users.noreply.github.com>
2018-07-29 16:21:06 -0400

    slack.py v1

---
+++ slack.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+import requests
+import sys
+
+URL = 'https://hooks.slack.com/services/XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX' #replace the X's with your actual webhook
+UN = 'ZABBIX'
+SLACK = ('#' + sys.argv[1])
+SUBJECT = sys.argv[2]
+MSG = sys.argv[3]
+
+if SUBJECT == 'RECOVERY':
+        EMOJI = ':smile:'
+        COLOR = '#2de52d'
+elif SUBJECT == 'OK':
+        EMOJI = ':smile:'
+        COLOR =  '#2de52d'
+elif SUBJECT == 'PROBLEM':
+        EMOJI = ':fire:'
+        COLOR = '#ff0000'
+else:
+        EMOJI = ':ghost:'
+        COLOR = '#ffee00'
+
+data = { "channel": SLACK,
+        "username": UN,
+      "icon_emoji": EMOJI,
+     "attachments": [{ "color": COLOR,
+            "text": MSG }]
+       }
+
+x = requests.post(URL, json=data)
